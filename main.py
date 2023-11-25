8ca1fdd25f9fac55c69d9727f0648d068b1a6726
John-Zaia <John.Zaia@outlook.com>
2023-11-24 19:31:17 -0800

    Added first command line interface input. Asks the user if he wants to encrypt or decrypt the message.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import cryptography
+
+loop=0
+
+#Loop to ask the user to user if he wants to encrypt or decrypt a file
+while loop == 0:
+    choice = input("Would you like to encrypt or decrypt a message. Valid choices ---> (encrypt/decrypt) ")
+    #if statement ignores case sensitivity
+    if(choice.lower() == "encrypt" or choice.lower() == "decrypt"):
+        loop = 1
+    else:
+        print("Please enter in a valid response")
