baa5b578ddaf4edf177c331bc91b618d7e3f2456
John-Zaia <John.Zaia@outlook.com>
2023-11-24 19:47:30 -0800

    Created if statement depeding on whether the user wants to encrypt or decrypt the message. Added command line inputs for the user to specify the key value, message, and file path/name. Also created empty functions which will be used to encrypt/decrypt the message.

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import cryptography
 
 loop=0
 
+#function that accepts a key value, message, and file path to encrypt the data
+def encryptMessage(key, message, path):
+    print()
+
+#function that accepts a key value and the path to decrypt the data
+def decryptMessage(key, path):
+    print()
+
 #Loop to ask the user to user if he wants to encrypt or decrypt a file
 while loop == 0:
     choice = input("Would you like to encrypt or decrypt a message. Valid choices ---> (encrypt/decrypt) ")
@@ -10,3 +18,14 @@ while loop == 0:
         loop = 1
     else:
         print("Please enter in a valid response")
+
+#if statement depending on if the user wants to encrypt or decrypt the message        
+if choice.lower() == "encrypt":
+    keyValue = input("Enter the key that you will be using to encrypt the message")
+    message = input("Enter the message you would like to encrypt")
+    fileName = input("Enter the name of the file you would like the encyrpted message to reside on")
+    encryptMessage(keyValue, message, fileName)
+else:
+    keyValue = input("Enter the key that you will be using to decrypt the file")
+    path = input("Enter the path or file name of the file you want to decyrpt")
+    decryptMessage(keyValue, path)
